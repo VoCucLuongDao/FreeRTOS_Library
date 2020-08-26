fb892fcb209fe026093132760e4007f61c202aad
VoCucLuongDao <55194741+VoCucLuongDao@users.noreply.github.com>
2020-08-26 13:43:43 +0700

    FreeRTOS STM32F4

--- inc/main.h
+++ inc/main.h
@@ -1,7 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include "delay.h"
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "task.h"
--- settings/Project.Debug.cspy.bat
+++ settings/Project.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\STM32\USB\settings\Project.Debug.general.xcl" --backend -f "D:\STM32\USB\settings\Project.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\STM32\FreeRTOS_Library\settings\Project.Debug.general.xcl" --backend -f "D:\STM32\FreeRTOS_Library\settings\Project.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\STM32\USB\settings\Project.Debug.general.xcl" "--debug_file=%~1" --backend -f "D:\STM32\USB\settings\Project.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\STM32\FreeRTOS_Library\settings\Project.Debug.general.xcl" "--debug_file=%~1" --backend -f "D:\STM32\FreeRTOS_Library\settings\Project.Debug.driver.xcl" 
 
 @echo off 
 :end
